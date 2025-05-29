cfc5f1e0c0958cdb5b9c321f0d2bf284ed9714a4
Riggs <riggs9162@gmx.de>
2025-05-30 00:05:22 +0200

    Update observer hooks for shared logic
    Moved `PlayerNoClip` and `OnPlayerObserver` functions from `sv_hooks.lua` to `sh_hooks.lua` to consolidate shared logic between server and client.

--- modules/observer/hooks/sh_hooks.lua
+++ modules/observer/hooks/sh_hooks.lua
@@ -6,6 +6,44 @@ function MODULE:CanPlayerObserve(client, state)
     return true
 end
 
+function MODULE:PlayerNoClip(client, desiredState)
+    if ( !hook.Run("CanPlayerObserve", client, desiredState) ) then
+        return false
+    end
+
+    if ( desiredState ) then
+        client:SetNoDraw(true)
+        client:DrawShadow(false)
+        client:SetNotSolid(true)
+
+        if ( SERVER ) then
+            client:SetNoTarget(true)
+        end
+    else
+        client:SetNoDraw(false)
+        client:DrawShadow(true)
+        client:SetNotSolid(false)
+
+        if ( SERVER ) then
+            client:SetNoTarget(false)
+        end
+    end
+
+    hook.Run("OnPlayerObserver", client, desiredState)
+
+    return true
+end
+
+function MODULE:OnPlayerObserver(client, state)
+    if ( CLIENT ) then return end
+    if ( !IsValid(client) or !client:IsPlayer() ) then return end
+
+    local logging = ax.module:Get("logging")
+    if ( logging ) then
+        logging:Send(client:Nick() .. " is now " .. (state and "observing" or "no longer observing") .. ".")
+    end
+end
+
 function MODULE:ShouldDrawObserverHUD(client)
     return true
 end
--- modules/observer/hooks/sv_hooks.lua
+++ modules/observer/hooks/sv_hooks.lua
@@ -1,39 +1,10 @@
 local MODULE = MODULE
 
-function MODULE:PlayerNoClip(client, desiredState)
-    if ( !hook.Run("CanPlayerObserve", client, desiredState) ) then
-        return false
-    end
-
-    if ( desiredState ) then
-        client:SetNoDraw(true)
-        client:DrawShadow(false)
-        client:SetNotSolid(true)
-        client:SetNoTarget(true)
-    else
-        client:SetNoDraw(false)
-        client:DrawShadow(true)
-        client:SetNotSolid(false)
-        client:SetNoTarget(false)
-    end
-
-    hook.Run("OnPlayerObserver", client, desiredState)
-
-    return true
-end
-
 function MODULE:EntityTakeDamage(target, dmgInfo)
     if ( !IsValid(target) or !target:IsPlayer() ) then return end
 
     if ( target:InObserver() ) then
         return true
-    end
-end
-
-function MODULE:OnPlayerObserver(client, state)
-    local logging = ax.module:Get("logging")
-    if ( logging ) then
-        logging:Send(client:Nick() .. " is now " .. (state and "observing" or "no longer observing") .. ".")
     end
 end
 
