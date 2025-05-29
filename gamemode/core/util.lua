44c1aa304986298df4d12e40c3ac851dc0c05d1b
bloodycop <eon.bloodycop@gmail.com>
2025-05-29 22:48:36 +0300

    Update util.lua

--- gamemode/core/util.lua
+++ gamemode/core/util.lua
@@ -335,10 +335,9 @@ end
 -- @param identifier any The identifier to search for.
 -- @return Player The player that was found.
 function ax.util:FindPlayer(identifier)
-    if ( identifier == nil ) then return nil end
+    if ( identifier == nil ) then return NULL end
 
-    local identifierType = type(identifier)
-    if ( identifierType == "Player" ) then
+    if ( type(identifier) == "Player" ) then
         return identifier
     end
 
@@ -349,7 +348,7 @@ function ax.util:FindPlayer(identifier)
     if ( istable(identifier) ) then
         for k, v in ipairs(identifier) do
             local foundPlayer = self:FindPlayer(v)
-            if ( foundPlayer ) then
+            if ( IsValid(foundPlayer) ) then
                 return foundPlayer
             end
         end
@@ -369,7 +368,7 @@ function ax.util:FindPlayer(identifier)
         end
     end
 
-    return nil
+    return NULL
 end
 
 --- Breaks a string into lines that fit within a maximum width in pixels.
