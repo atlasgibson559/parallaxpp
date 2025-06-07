136ec004563e9baf0f0cbd3a1353622141fe0ff7
riggs9162 <riggs9162@gmx.de>
2025-06-07 20:19:50 +0200

    Delete test-module.lua

--- modules/test-module.lua
+++
@@ -1,9 +0,0 @@
-local MODULE = MODULE
-
-MODULE.Name = "Test Module"
-MODULE.Description = "A test module."
-MODULE.Author = "Riggs"
-
-function MODULE:OnReloaded()
-    ax.util:Print(self.Name .. " has been reloaded.")
-end
