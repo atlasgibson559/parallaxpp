e4cac3eda6e59e42647b0274e738fab0e9f7df26
riggs9162 <riggs9162@gmx.de>
2025-06-07 18:00:13 +0200

    Add title to scoreboard panel
    Introduced a title element to the scoreboard panel with the text 'SCOREBOARD', using the 'parallax.title' font.

--- gamemode/core/ui/cl_tab_scoreboard.lua
+++ gamemode/core/ui/cl_tab_scoreboard.lua
@@ -5,6 +5,11 @@ local PANEL = {}
 function PANEL:Init()
     self:Dock(FILL)
 
+    local title = self:Add("ax.text")
+    title:Dock(TOP)
+    title:SetFont("parallax.title")
+    title:SetText("SCOREBOARD")
+
     local scoreboard = self:Add("ax.scoreboard")
     scoreboard:Dock(FILL)
 end
