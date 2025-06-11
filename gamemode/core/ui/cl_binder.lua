69a23f2aa36cd238a3e9e70d0d0830288553d8a2
bloodycop <eon.bloodycop@gmail.com>
2025-06-11 23:30:35 +0300

    @riggs9162 pls make binder for options

---
+++ gamemode/core/ui/cl_binder.lua
@@ -0,0 +1,8 @@
+local PANEL = {}
+DEFINE_BASECLASS("DBinder")
+
+-- Check: https://wiki.facepunch.com/gmod/input.StartKeyTrapping
+-- Check: https://github.com/Facepunch/garrysmod/blob/master/garrysmod/lua/vgui/dbinder.lua
+
+
+vgui.Register("ax.binder", PANEL, "DBinder")
