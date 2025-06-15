f8709de191bfba995e79cd99d39fe739968fd55e
Riggs <riggs9162@gmx.de>
2025-06-15 23:24:56 +0200

    Add custom key binder panel ax.binder
    Implements a new vgui panel 'ax.binder' for binding a single key input, replacing the base class from DBinder to ax.button. The panel provides methods for handling key input, updating display text, and optional change callbacks.

--- gamemode/core/ui/cl_binder.lua
+++ gamemode/core/ui/cl_binder.lua
@@ -9,11 +9,71 @@
     Attribution is required. If you use or modify this file, you must retain this notice.
 ]]
 
+--- ax.binder
+-- A simple key binder panel for binding a single key input.
+-- @panel ax.binder
+
 local PANEL = {}
-DEFINE_BASECLASS("DBinder")
 
--- Check: https://wiki.facepunch.com/gmod/input.StartKeyTrapping
--- Check: https://github.com/Facepunch/garrysmod/blob/master/garrysmod/lua/vgui/dbinder.lua
+AccessorFunc(PANEL, "m_iSelectedNumber", "SelectedNumber", FORCE_NUMBER)
 
+function PANEL:Init()
+    self:SetText("")
+    self:SetFont("parallax")
+    self:SetContentAlignment(5)
+    self:SetDrawBackground(true)
+    self:SetDrawBorder(true)
+    self:SetPaintBackgroundEnabled(true)
+    self:SetPaintBorderEnabled(true)
+    self:SetSelectedNumber(0)
+    self:SetMouseInputEnabled(true)
+    self:SetKeyBoardInputEnabled(false)
 
-vgui.Register("ax.binder", PANEL, "DBinder")
+    self.Changed = false
+end
+
+function PANEL:DoClick()
+    self:GetParent():RequestFocus()
+    self:SetText("...")
+    self.Changed = true
+    self:SetKeyboardInputEnabled(true)
+end
+
+function PANEL:OnKeyCodeTyped(code)
+    if ( code == KEY_ESCAPE ) then
+        self:SetText(input.GetKeyName(self:GetSelectedNumber()))
+        self:SetKeyboardInputEnabled(false)
+        self.Changed = false
+        return
+    end
+
+    self:SetSelectedNumber(code)
+    self:SetText(input.GetKeyName(code))
+    self:SetKeyboardInputEnabled(false)
+    self.Changed = true
+
+    if ( self.OnChange ) then
+        self:OnChange(code)
+    end
+end
+
+function PANEL:OnChange(code)
+    -- Optional override
+end
+
+function PANEL:UpdateText()
+    self:SetText(input.GetKeyName(self:GetSelectedNumber()))
+end
+
+function PANEL:AllowEnter(bAllow)
+    self.AllowEnter = bAllow
+end
+
+function PANEL:Think()
+    if ( self.Changed and !self:HasKeyboardInputEnabled() ) then
+        self.Changed = false
+        self:UpdateText()
+    end
+end
+
+vgui.Register("ax.binder", PANEL, "ax.button")
