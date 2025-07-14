4dde691da395f0cec4b73512668df8b5169b1c49
bloodycop <89155277+bloodycop6385@users.noreply.github.com>
2025-07-14 20:04:54 +0300

    Invert order

--- gamemode/core/libraries/shared/sh_localization.lua
+++ gamemode/core/libraries/shared/sh_localization.lua
@@ -138,7 +138,7 @@ if ( CLIENT ) then
     cvars.AddChangeCallback("gmod_language", function(convar, oldValue, newValue)
         for i = 1, #ax.gui do
             local v = ax.gui[i]
-            if ( ispanel(v) and IsValid(v) ) then
+            if ( IsValid(v) and ispanel(v) ) then
                 local className = v:GetClassName()
                 v:Remove()
 
